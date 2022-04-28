d44fae26a878addd96480c96ecd87762f8e1403b
maco-data <mcospino@gmail.com>
2022-04-28 22:21:08 +0200

    Complete day 06 project

---
+++ day_06/day_06_project.py
@@ -0,0 +1,20 @@
+""" This is just copy from the Reeborg code, the errors
+displayed are due to many functions called without
+having them defined """
+
+# This function makes the robot turn itself until it faces right
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+# Loop that helps the robot reach the goal    
+while not at_goal():
+    if right_is_clear(): # if the right is clear
+        turn_right()
+        move()
+    elif front_is_clear(): # if there is nothing in the front
+        move()
+    else:
+        turn_left()
